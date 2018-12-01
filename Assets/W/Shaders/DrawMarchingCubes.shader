7055b25a5d714f3513e43f7da4870bc2030e49c5
khlebousa <khlebousa@gmail.com>
2018-12-01 09:53:43 +0100

    saturday morning

---
+++ Assets/W/Shaders/DrawMarchingCubes.shader
@@ -0,0 +1,18 @@
+﻿Shader "MarchingCubesGPUProject/DrawMarchingCubes"
+{
+	SubShader{
+	  Tags { "RenderType" = "Opaque" }
+	  CGPROGRAM
+	  #pragma surface surf Lambert
+
+	  struct Input {
+		float4 color : COLOR;
+	  };
+
+	  void surf(Input IN, inout SurfaceOutput o) {
+		  o.Albedo = IN.color.xyz;
+	  }
+	  ENDCG
+	}
+		Fallback "Diffuse"
+}
---
+++ Assets/W/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using Assets.MarchingCubesGPU.Scripts;
+using MarchingCubesGPUProject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //public MarchingCubesGPUProject.Terrain terrain;
+    public Transform template;
+	//public MonoBehaviour brush;
+	// Use this for initialization
+	void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//var dt = Time.deltaTime;
+		//terrain.brush.transform.position += new Vector3(0.0f, 0.3f, 0.0f) * dt;1
+
+		transform.position = template.position;
+		Debug.Log(template.position);
+		//transform.rotation = template.rotation;
+
+	}
+}

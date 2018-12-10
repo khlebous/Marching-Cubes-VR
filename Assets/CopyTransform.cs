4c521a21831098810ad2c2b3b7eaa240a6446072
khlebousa <khlebousa@gmail.com>
2018-12-10 16:34:48 +0100

    add video capture

---
+++ Assets/CopyTransform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+	[SerializeField] private Transform toFollow;
+	private Transform mytransform;
+
+	void Start ()
+	{
+		mytransform = GetComponent<Transform>();
+	}
+	
+	void Update ()
+	{
+		mytransform.position = toFollow.position;
+		mytransform.rotation = toFollow.rotation;
+	}
+}

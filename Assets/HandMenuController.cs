37f2f3d6440998daada4e251fc8c11843d5153e2
khlebousa <khlebousa@gmail.com>
2018-12-10 15:58:01 +0100

    hand menu object

---
+++ Assets/HandMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMenuController : MonoBehaviour
+{
+	void Update()
+	{
+		Vector2 tmp = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+		if (Mathf.Abs(tmp.x) > Mathf.Abs(tmp.y))
+		{
+			if (tmp.x > 0.5)
+				Debug.Log("right");
+			else if (tmp.x <= -1)
+				Debug.Log("left");
+		}
+		else
+		{
+			if (tmp.y == 1 )
+				Debug.Log("up");
+			else if (tmp.y == -1)
+				Debug.Log("down");
+		}
+	}
+}

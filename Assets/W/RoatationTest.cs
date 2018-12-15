acbcdc1cc173fd9e69efb9dde8f2f80a7d5aa449
Unknown <wojtek.trze@gmail.com>
2018-12-15 22:49:40 +0100

    functions for rotation tests

--- Assets/W/RoatationTest.cs
+++ Assets/W/RoatationTest.cs
@@ -13,9 +13,9 @@ public class RoatationTest
         var currentVec = currentPos - startTerrainTransform.position;
 
         var rotationChange = Quaternion.FromToRotation(startVec, currentVec).eulerAngles;
-        var rotaiton = startTerrainTransform.rotation.eulerAngles + rotationChange;
+        var rotation = startTerrainTransform.rotation.eulerAngles + rotationChange;
 
-        return rotaiton;
+        return rotation;
     }
 
     public Vector3 GetRotation_2(Transform startTerrainTransform, Vector3 startPos, Vector3 currentPos)
@@ -24,10 +24,10 @@ public class RoatationTest
         var currentVec = currentPos - startTerrainTransform.position;
 
         var rotationChange = Quaternion.FromToRotation(startVec, currentVec).eulerAngles;
-        var rotaiton = startTerrainTransform.rotation.eulerAngles;
-        rotaiton.y += rotationChange.y;
+        var rotation = startTerrainTransform.rotation.eulerAngles;
+        rotation.y += rotationChange.y;
 
-        return rotaiton;
+        return rotation;
     }
 
 
@@ -37,14 +37,20 @@ public class RoatationTest
         var currentVec = currentPos - startTerrainTransform.position;
 
         var rotationChange = Quaternion.FromToRotation(startVec, currentVec).eulerAngles;
-        var rotaiton = startTerrainTransform.rotation.eulerAngles;
+        var rotation = startTerrainTransform.rotation.eulerAngles;
 
-        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x))
-            rotaiton.y += rotationChange.y;
+        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x)
+            && Math.Abs(rotationChange.y) > Math.Abs(rotationChange.z))
+        {
+            rotation.y += rotationChange.y;
+        }
         else
-            rotaiton.x += rotationChange.x;
+        {
+            rotation.x += rotationChange.x;
+            rotation.z += rotationChange.z;
+        }
 
-        return rotaiton;
+        return rotation;
     }
 
 
@@ -55,14 +61,28 @@ public class RoatationTest
         var currentVec = currentPos - startTerrainTransform.position;
 
         var rotationChange = Quaternion.FromToRotation(startVec, currentVec).eulerAngles;
-        var rotaiton = startTerrainTransform.rotation.eulerAngles;
+        var rotation = startTerrainTransform.rotation.eulerAngles;
 
-        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x))
-            rotaiton.y += rotationChange.y;
+        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x)
+            && Math.Abs(rotationChange.y) > Math.Abs(rotationChange.z))
+        {
+            rotation.y += rotationChange.y;
+        }
         else
-            rotaiton.x += rotationChange.x;
+        {
+            rotation.x += rotationChange.x;
+            rotation.z += rotationChange.z;
+        }
 
-        return rotaiton;
+        var boundaryAngle = 10f;
+
+        if (Math.Abs(rotation.x) < boundaryAngle && Math.Abs(rotation.z) < boundaryAngle)
+        {
+            rotation.x = 0;
+            rotation.z = 0;
+        }
+
+        return rotation;
     }
 
 
@@ -72,15 +92,31 @@ public class RoatationTest
         var currentVec = currentPos - startTerrainTransform.position;
 
         var rotationChange = Quaternion.FromToRotation(startVec, currentVec).eulerAngles;
-        var rotaiton = startTerrainTransform.rotation.eulerAngles;
+        var rotation = startTerrainTransform.rotation.eulerAngles;
 
 
-        //sth with max
-        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x))
-            rotaiton.y += rotationChange.y;
+        if (Math.Abs(rotationChange.y) > Math.Abs(rotationChange.x)
+            && Math.Abs(rotationChange.y) > Math.Abs(rotationChange.z))
+        {
+            rotation.y += rotationChange.y;
+        }
         else
-            rotaiton.x += rotationChange.x;
+        {
+            rotation.x += rotationChange.x;
+            rotation.z += rotationChange.z;
+        }
 
-        return rotaiton;
+        var boundaryAngle = 10f;
+
+        if (Math.Abs(rotation.x) < boundaryAngle && Math.Abs(rotation.z) < boundaryAngle)
+        {
+            rotation.x = 0;
+            rotation.z = 0;
+        }
+
+        rotation = Vector3.Max(rotation, new Vector3(-90, rotation.x, -90));
+        rotation = Vector3.Min(rotation, new Vector3(90, rotation.x, 90));
+
+        return rotation;
     }
 }

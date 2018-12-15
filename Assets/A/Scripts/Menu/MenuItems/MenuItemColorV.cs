9ec51cdd9b0236f0f75b60a91e191b78dea23b4f
khlebousa <khlebousa@gmail.com>
2018-12-15 19:07:16 +0100

    working menu prototype

---
+++ Assets/A/Scripts/Menu/MenuItems/MenuItemColorV.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemColorV : MenuItemV
+{
+	private bool colorIsChoosing = true;
+
+	private buttonState thumbstick = buttonState.Normal;
+	[SerializeField] private bool active;
+	[SerializeField] Draggable color;
+	//[SerializeField] Draggable intensity;
+
+	public void Start()
+	{
+		active = false;
+	}
+
+	public override void SetActive()
+	{
+		base.SetActive();
+		active = true;
+	}
+
+	public override void SetInactive()
+	{
+		base.SetInactive();
+		active = false;
+	}
+
+	void Update()
+	{
+		if (active)
+		{
+			if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft))
+			{
+				color.Input(buttonState.Left);
+				Debug.Log("Left");
+
+			}
+			else if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight))
+			{
+				color.Input(buttonState.Right);
+				Debug.Log("Right");
+			}
+			else
+			{
+				if (thumbstick != buttonState.Normal)
+					thumbstick = buttonState.Normal;
+			}
+		}
+	}
+
+}

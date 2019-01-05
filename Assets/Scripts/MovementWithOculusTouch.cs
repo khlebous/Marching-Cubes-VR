f28776842fc888364c6b1610db417931ad466b03
Unknown <wojtek.trze@gmail.com>
2019-01-05 16:31:29 +0100

    refactor

--- Assets/Scripts/MovementWithOculusTouch.cs
+++ Assets/Scripts/MovementWithOculusTouch.cs
@@ -3,77 +3,77 @@ using UnityEngine;
 
 public class MovementWithOculusTouch : MonoBehaviour
 {
-	[Header("Oculus Touch input")]
-	[Tooltip("Button to move")]
-	[SerializeField] private OVRInput.Button buttonY = OVRInput.Button.Start;
-	[Tooltip("Controller to follow")]
+    [Header("Oculus Touch input")]
+    [Tooltip("Button to move")]
+    [SerializeField] private OVRInput.Button buttonY = OVRInput.Button.Start;
+    [Tooltip("Controller to follow")]
     [SerializeField] private Transform controllerToFollow;
 
     [Header("Rotatation multipliers")]
-	[SerializeField] private int speed = 10;
-	[SerializeField] private int bound = 20;
+    [SerializeField] private int speed = 10;
+    [SerializeField] private int bound = 20;
 
-	private new Transform transform;
-	private Vector3 startPosA;
+    private new Transform transform;
+    private Vector3 startPos;
 
-	private Coroutine buttonA_down;
-	private Coroutine buttonA_up;
+    private Coroutine button_down;
+    private Coroutine button_up;
 
-	void Start ()
-	{
-		transform = GetComponent<Transform>();
-		startPosA = Vector3.zero;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        startPos = Vector3.zero;
 
-		StartListening();
-	}
+        StartListening();
+    }
 
-	private void StartListening()
-	{
-		buttonA_down = StartCoroutine(WaitForButtonA_Down());
-	}
+    private void StartListening()
+    {
+        button_down = StartCoroutine(WaitForButton_Down());
+    }
 
-	private void StopListening()
-	{
-		if (null != buttonA_down)
-			StopCoroutine(buttonA_down);
+    private void StopListening()
+    {
+        if (null != button_down)
+            StopCoroutine(button_down);
 
-		if (null != buttonA_up)
-			StopCoroutine(buttonA_up);
-	}
+        if (null != button_up)
+            StopCoroutine(button_up);
+    }
 
-	private IEnumerator WaitForButtonA_Down()
-	{
-		while (true)
-		{
-			if (OVRInput.GetDown(buttonY))
-			{
-                startPosA = controllerToFollow.position;
+    private IEnumerator WaitForButton_Down()
+    {
+        while (true)
+        {
+            if (OVRInput.GetDown(buttonY))
+            {
+                startPos = controllerToFollow.position;
 
-                StopCoroutine(buttonA_down);
-				buttonA_up = StartCoroutine(WaitForButtonA_Up());
-			}
+                StopCoroutine(button_down);
+                button_up = StartCoroutine(WaitForButton_Up());
+            }
 
-			yield return new WaitForEndOfFrame();
-		}
-	}
+            yield return new WaitForEndOfFrame();
+        }
+    }
 
-	private IEnumerator WaitForButtonA_Up()
-	{
-		while (true)
-		{
-			Vector3 currentPos = controllerToFollow.position;
-            Vector3 diff = startPosA - currentPos;
-			startPosA = currentPos;
-			transform.Translate(-diff*speed);
+    private IEnumerator WaitForButton_Up()
+    {
+        while (true)
+        {
+            Vector3 currentPos = controllerToFollow.position;
+            Vector3 diff = currentPos - startPos;
+            startPos = currentPos;
+            transform.Translate(diff * speed);
 
-			if (OVRInput.GetUp(buttonY))
-			{
-				StopCoroutine(buttonA_up);
+            if (OVRInput.GetUp(buttonY))
+            {
+                StopCoroutine(button_up);
 
-				buttonA_down = StartCoroutine(WaitForButtonA_Down());
-			}
+                button_down = StartCoroutine(WaitForButton_Down());
+            }
 
-			yield return new WaitForEndOfFrame();
-		}
-	}
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }

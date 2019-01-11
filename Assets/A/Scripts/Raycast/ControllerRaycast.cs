c545a801510a8fda7fda9629a2ba1a50c156fc54
Wojtek <wojtek.trze@gmail.com>
2019-01-11 23:34:05 +0100

    controller raycast refactor, camera capture basic

--- Assets/A/Scripts/Raycast/ControllerRaycast.cs
+++ Assets/A/Scripts/Raycast/ControllerRaycast.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class RaycatTesting : MonoBehaviour
+public class ControllerRaycast : MonoBehaviour
 {
 	public LineRenderer laserLineRenderer;
 	public float maxRayDistance = 20f;
@@ -12,22 +10,14 @@ public class RaycatTesting : MonoBehaviour
 	{
 		Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
 		laserLineRenderer.SetPositions(initLaserPositions);
-		//laserLineRenderer.SetWidth(0.1f, 0.1f);
 		laserLineRenderer.startWidth = 0.1f;
 		laserLineRenderer.endWidth = 0.01f;
 	}
 
 	void Update()
 	{
-		if (true)
-		{
-			ShootLaserFromTargetPosition(transform.position, transform.forward, maxRayDistance);
-			laserLineRenderer.enabled = true;
-		}
-		else
-		{
-			laserLineRenderer.enabled = false;
-		}
+		ShootLaserFromTargetPosition(transform.position, transform.forward, maxRayDistance);
+		laserLineRenderer.enabled = true;
 	}
 
 	void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
@@ -46,14 +36,4 @@ public class RaycatTesting : MonoBehaviour
 		laserLineRenderer.SetPosition(1, endPosition);
 		Debug.DrawLine(transform.position, transform.position + direction * maxRayDistance, Color.red);
 	}
-
-	//private void FixedUpdate()
-	//{
-	//	Ray ray = new Ray(transform.position, transform.forward);
-	//	RaycastHit[] hits = Physics.RaycastAll(ray, maxRayDistance, activeLayers);
-
-	//	Debug.DrawLine(transform.position, transform.position + transform.forward * maxRayDistance, Color.red);
-
-
-	//}
 }

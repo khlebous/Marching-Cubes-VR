4bc685d4c307052b10c06490b8e22f061b4e9cb1
Unknown <wojtek.trze@gmail.com>
2019-01-17 20:03:20 +0100

    fix deleting model

--- Assets/W/Scripts/EditableScene.cs
+++ Assets/W/Scripts/EditableScene.cs
@@ -7,78 +7,78 @@ using UnityEngine;
 
 public class EditableScene : MonoBehaviour
 {
-	public Guid Guid { get; set; }
+    public Guid Guid { get; set; }
 
-	public McGameObjData Terrain { get; set; }
-	public List<McObject> ModelsOnTerrain { get; set; }
-	public Dictionary<Guid, McGameObjData> Models { get; set; }
+    public McGameObjData Terrain { get; set; }
+    public List<McObject> ModelsOnTerrain { get; set; }
+    public Dictionary<Guid, McGameObjData> Models { get; set; }
 
-	public GameObject InstantiateModel(Guid guid)
-	{
-		var obj = GameObject.Instantiate(Models[guid].GameObject);
+    public GameObject InstantiateModel(Guid guid)
+    {
+        var obj = GameObject.Instantiate(Models[guid].GameObject);
         obj.name = McConsts.ObjectPrefix + guid.ToString();
         obj.transform.parent = this.transform;
-		obj.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-		obj.transform.localPosition = Vector3.zero;
-		ModelsOnTerrain.Add(new McObject(guid, obj));
+        obj.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        obj.transform.localPosition = Vector3.zero;
+        ModelsOnTerrain.Add(new McObject(guid, obj));
 
-		obj.AddComponent<ObjectController>();
-		obj.AddComponent<MovementWithOculusTouch>();
-		obj.GetComponent<MovementWithOculusTouch>().enabled = false;
-		obj.AddComponent<RotationWithOculusTouch>();
-		obj.GetComponent<RotationWithOculusTouch>().enabled = false;
-		obj.AddComponent<ScaleWithOculusTouch>();
-		obj.GetComponent<ScaleWithOculusTouch>().enabled = false;
+        obj.AddComponent<ObjectController>();
+        obj.AddComponent<MovementWithOculusTouch>();
+        obj.GetComponent<MovementWithOculusTouch>().enabled = false;
+        obj.AddComponent<RotationWithOculusTouch>();
+        obj.GetComponent<RotationWithOculusTouch>().enabled = false;
+        obj.AddComponent<ScaleWithOculusTouch>();
+        obj.GetComponent<ScaleWithOculusTouch>().enabled = false;
 
-		obj.tag = Constants.OBJECT_TAG;
-		obj.SetActive(true);
+        obj.tag = Constants.OBJECT_TAG;
+        obj.SetActive(true);
 
-		return obj;
-	}
-	public void SetOrUpdateTerrain(McGameObjData data)
-	{
-		var newTransform = data.GameObject.transform;
-		newTransform.parent = this.transform;
+        return obj;
+    }
+    public void SetOrUpdateTerrain(McGameObjData data)
+    {
+        var newTransform = data.GameObject.transform;
+        newTransform.parent = this.transform;
 
-		if (Terrain != null)
-		{
-			var currTransform = Terrain.GameObject.transform;
+        if (Terrain != null)
+        {
+            var currTransform = Terrain.GameObject.transform;
 
-			newTransform.position = currTransform.position;
-			newTransform.rotation = currTransform.rotation;
-			newTransform.localScale = currTransform.localScale;
+            newTransform.position = currTransform.position;
+            newTransform.rotation = currTransform.rotation;
+            newTransform.localScale = currTransform.localScale;
 
-			GameObject.Destroy(Terrain.GameObject);
-		}
+            GameObject.Destroy(Terrain.GameObject);
+        }
 
-		Terrain = data;
-	}
-	public void SetOrUpdateModel(McGameObjData data)
-	{
-		var guid = data.Data.Guid;
-		data.GameObject.transform.parent = this.transform;
+        Terrain = data;
+    }
+    public void SetOrUpdateModel(McGameObjData data)
+    {
+        var guid = data.Data.Guid;
+        data.GameObject.transform.parent = this.transform;
 
-		if (!Models.ContainsKey(guid))
-		{
-			Models[guid] = data;
-		}
-		else
-		{
-			GameObject.Destroy(Models[guid].GameObject);
+        if (!Models.ContainsKey(guid))
+        {
+            Models[guid] = data;
+        }
+        else
+        {
+            GameObject.Destroy(Models[guid].GameObject);
 
-			Models[guid] = data;
-			UpdateModelsOnTerrain(guid);
-		}
-	}
-	public void UpdateModelsOnTerrain(Guid modelGuid)
-	{
-		var toUpdate = ModelsOnTerrain.Where(x => x.ModelGuid == modelGuid).ToList();
-		foreach (var model in toUpdate)
-		{
-			var updatedObj = InstantiateModel(modelGuid);
-			updatedObj.transform.position = model.GameObject.transform.position;
-			updatedObj.transform.rotation = model.GameObject.transform.rotation;
-			updatedObj.transform.localScale = model.GameObject.transform.localScale;
+            Models[guid] = data;
+            UpdateModelsOnTerrain(guid);
+        }
+    }
+    public void UpdateModelsOnTerrain(Guid modelGuid)
+    {
+        var toUpdate = ModelsOnTerrain.Where(x => x.ModelGuid == modelGuid).ToList();
+        foreach (var model in toUpdate)
+        {
+            var updatedObj = InstantiateModel(modelGuid);
+            updatedObj.transform.position = model.GameObject.transform.position;
+            updatedObj.transform.rotation = model.GameObject.transform.rotation;
+            updatedObj.transform.localScale = model.GameObject.transform.localScale;
 
             GameObject.Destroy(model.GameObject);
             model.GameObject = updatedObj;
@@ -90,9 +90,10 @@ public class EditableScene : MonoBehaviour
         foreach (var obj in objToDelete)
         {
             GameObject.Destroy(obj.GameObject);
+            ModelsOnTerrain.Remove(obj);
         }
 
-		GameObject.Destroy(Models[modelGuid].GameObject);
+        GameObject.Destroy(Models[modelGuid].GameObject);
         Models.Remove(modelGuid);
     }
     public void DeleteModelFromTerrain(GameObject gameObject)
@@ -115,21 +116,21 @@ public class EditableScene : MonoBehaviour
         }
     }
 
-	public McSceneData GetData()
-	{
-		var data = new McSceneData();
-		data.Guid = Guid;
-		data.TerrainGuid = Terrain.Data.Guid;
-		data.Models = ModelsOnTerrain.Select(x => new McSceneModelData()
-		{
-			Guid = x.ModelGuid,
-			Position = x.GameObject.transform.localPosition,
-			Rotation = x.GameObject.transform.localRotation.eulerAngles,
-			Scale = x.GameObject.transform.localScale,
-		}).ToList();
+    public McSceneData GetData()
+    {
+        var data = new McSceneData();
+        data.Guid = Guid;
+        data.TerrainGuid = Terrain.Data.Guid;
+        data.Models = ModelsOnTerrain.Select(x => new McSceneModelData()
+        {
+            Guid = x.ModelGuid,
+            Position = x.GameObject.transform.localPosition,
+            Rotation = x.GameObject.transform.localRotation.eulerAngles,
+            Scale = x.GameObject.transform.localScale,
+        }).ToList();
 
-		return data;
-	}
+        return data;
+    }
 
     public void Destroy()
     {
